4e26632fd06390c21457f0fe88a26cac530a16fe
lakshya Agarwal <agarwallakshya99@gmail.com>
2025-01-12 10:49:15 +0530

    substring pattern matching implemented

--- matchsubstring.c
+++ matchsubstring.c
@@ -10,9 +10,6 @@ char *dynamicinputgenerator(char *str, int *strlength)
     {
         (*strlength)++;
         str = (char *)realloc(str, sizeof(char) * ((*strlength) + 1));
-        if (ch == ' ')
-            str[position] = ' ';
-        else
             str[position] = ch;
         position++;
     }
@@ -25,9 +22,67 @@ char *dynamicinputgenerator(char *str, int *strlength)
     return str;
 }
 
-int sustringinstring(char *textstring,int textlength,char *patternstring,int patternlength)
+int *generateLPS(char *patternstring,int patternlength)
 {
-    
+     int *LPS=(int *)malloc(sizeof(int)*(patternlength+1));
+    LPS[0]=0;
+    int len=0;
+    int i=1;
+    while(i<patternlength)
+    {
+        if(patternstring[i]==patternstring[len])
+        {
+            len++;
+            LPS[i]=len;
+            i++;
+        }
+        else if(len!=0)
+        {
+            len=LPS[len-1];
+        }
+        else
+        {
+            LPS[i]=0;
+            i++;
+        }
+    }
+    return LPS;
+   
+}
+
+int substringinstring(char *textstring,int textlength,char *patternstring,int patternlength)
+{
+    int *LPS=NULL;
+    LPS=generateLPS(patternstring,patternlength);
+    if(LPS==NULL)
+    printf("Lps is not working correctly");
+    int i=0;
+    int j=0;
+    int index=-1;
+    while(i<textlength)
+    {
+        if(textstring[i]==patternstring[j])
+        {
+            i++;
+            j++;
+            if(j==patternlength)
+            {
+                index= i-j;
+                break;
+            }
+            
+        }
+        else
+        {
+            if(j!=0)
+            j=LPS[j-1];
+            else
+            i++;
+        }
+    }
+    free(LPS);
+    return index;
+
 }
 int main()
 {
