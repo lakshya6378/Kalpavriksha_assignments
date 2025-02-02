cde1ff37eeed511f75442f7cdc63d4edcd32948c
lakshya agarwal <agarwallakshya99@gmail.com>
2025-02-02 13:45:56 +0000

    queue using one stack is implemented

---
+++ queueUsingOneStack.c
@@ -0,0 +1,162 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX_STACK_SIZE 1000
+typedef struct Stack{
+    int st[MAX_STACK_SIZE];
+    int top;
+} stack;
+void push(stack *sta,int val)
+{
+    if(sta->top==MAX_STACK_SIZE-1)
+    {
+        printf("stack is full, push is not possible");
+        return ;
+    }
+    sta->top++;
+    sta->st[sta->top]=val;
+}
+
+void pop(stack *sta)
+{
+    if(sta->top==-1)
+    {
+        printf("stack is already empty");
+        return ;
+    }
+    sta->top--;
+    return;
+
+}
+
+int peekS(stack *sta)
+{
+    if(sta->top==-1)
+    {
+        printf("stack is empty");
+        return -1;
+    }
+    return sta->st[sta->top];
+}
+
+int isSEmpty(stack *sta)
+{
+      return sta->top==-1;
+}
+
+int sizeS(stack *sta)
+{
+    return sta->top+1;
+}
+typedef struct queue{
+    stack sta;
+} Queue;
+Queue qu;
+void InsertAtBottomStack(stack *sta,int val)
+{
+    if(isSEmpty(sta))
+    {
+         push(sta,val);
+         return;
+    }
+    int value=peekS(sta);
+    pop(sta);
+    InsertAtBottomStack(sta,val);
+    push(sta,value);
+}
+void enqueue(int val)
+{
+    if(sizeS(&qu.sta)==MAX_STACK_SIZE)
+    {
+        printf("cant insert queue is full");
+        return;
+    }
+    if(isSEmpty(&qu.sta))
+    push(&qu.sta,val);
+    else
+    {
+        InsertAtBottomStack(&qu.sta,val);
+    }
+    printf("%d inserted successfully",val);
+}
+
+void dequeue()
+{
+    if(isSEmpty(&qu.sta))
+    {
+        printf("queue is already empty");
+        return;
+    }
+    int val=peekS(&qu.sta);
+    pop(&qu.sta);
+    printf("%d is deleted successfully",val);
+}
+int isEmpty()
+{
+    return isSEmpty(&qu.sta);
+}
+int peek()
+{
+    if(isSEmpty(&qu.sta))
+    {
+        printf("queue is empty");
+        return -1;
+    }
+    return peekS(&qu.sta);
+}
+int size()
+{
+    if(isSEmpty(&qu.sta))
+    return 0;
+    return sizeS(&qu.sta);
+}
+int main()
+{
+    qu.sta.top=-1;
+    int choice=0;
+    int value;
+    do 
+    {
+        printf("\nenter a operation to perform\n");
+        printf("1. enter 1 for enqueue operation\n");
+        printf("2.enter 2 for dequeue operation\n");
+        printf("3.enter 3 for peek operation\n");
+        printf("4.enter 4 for checking queue is empty or not\n");
+        printf("5.enter 5 for check queue size\n");
+        printf("6.enter 6 for exit\n");
+        scanf("%d",&choice);
+        switch(choice)
+        {
+            case 1:
+                  printf("enter a value");
+                  scanf("%d",&value);
+                  enqueue(value);
+                  break;
+            case 2:
+                dequeue();
+                break;
+            case 3:
+                 value=peek();
+                 printf("%d",value);
+                 break;
+            case 4:
+                value=isEmpty();
+                if(value==1)
+                printf("queue is empty");
+                else
+                printf("queue is not empty");
+            break;
+            case 5:
+                value=size();
+                printf("size of queue is %d",value);
+                break;
+            case 6:
+                printf("terminating the program...");
+                break;
+            default:
+               printf("enter a valid choice");
+               break;
+
+        }
+    }while(choice!=6);
+    
+}
